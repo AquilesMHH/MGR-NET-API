fe9e45b35240211cac9a0ed29c0f416d5aa71215
nereoaquiles <37483062+nereoaquiles@users.noreply.github.com>
2018-06-10 22:22:23 -0500

    jj

---
+++ MGR-Business/com.pe.mgr.service/VariacionesService.cs
@@ -0,0 +1,14 @@
+﻿using MGR_Entidades.Entidad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MGR_Business.com.pe.mgr.service
+{
+    public interface VariacionesService
+    {
+        List<VariacionDto> listar(String nombreTabla, String claveRegistro);
+    }
+}
