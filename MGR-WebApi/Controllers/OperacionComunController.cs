dd5d6264cff44d7a9c5511cabfa112114b254b8c
nereoaquiles <37483062+nereoaquiles@users.noreply.github.com>
2018-06-17 22:09:16 -0500

    gg

--- MGR-WebApi/Controllers/OperacionComunController.cs
+++ MGR-WebApi/Controllers/OperacionComunController.cs
@@ -10,6 +10,26 @@ namespace MGR_WebApi.Controllers
     [RoutePrefix("rest/operacioncomun")]
     public class OperacionComunController : ApiController
     {
+        [Route("consultardescripcioncatalogo", Name = "Detall consultardescripcioncatalogo")]
+        [HttpGet]
+        public String consutlarDescripcionCatalogo([FromUri]  int variable, string codigo)
+        {
+            return null;
+        }
 
+        [Route("autocompletarparacompendio", Name = "Detall autocompletarparacompendio")]
+        [HttpGet]
+        public String autocompletarParaCompendio([FromUri]  int variable, string codigo)
+        {
+            return null;
+        }
+
+
+        [Route("consultardescripcionfiltro", Name = "Detall consultardescripcionfiltro")]
+        [HttpGet]
+        public String consultarDescripcionFiltro([FromUri]  int variable, string codigo)
+        {
+            return null;
+        }
     }
 }
